8748abf63bca3b5e2fa1d3ad594ff50dd83e4000
Ashu <ayushprajapati272007@gmail.com>
2026-06-11 17:28:21 +0530

    Few basics of JS

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log("2">0);
+// console.log("5"<0);
+
+/*
+Avoid these type of operations
+{
+    console.log(null > 0)
+    console.log(null == 0)
+    console.log(null >= 0)
+    
+    console.log(undefined > 0)
+    console.log(undefined < 0)
+    console.log(undefined = 0)
+}
+*/
+
+// ===
+
+console.log("2"===2)
