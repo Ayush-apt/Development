d87a4d8ed6179d0cb92fbad0d09edf1647c1378f
Ashu <ayushprajapati272007@gmail.com>
2026-06-28 17:44:53 +0530

    Num & Math

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -1,29 +1,29 @@
 // Numbers 
 
-// const score = 500
-// console.log(score)
+const score = 500
+console.log(score)
 
-// const balance = new Number(100)
-// console.log(balance)
+const balance = new Number(100)
+console.log(balance)
 
-// console.log(balance.toString().length)
-// console.log(balance.toFixed(2))
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
 
-// const otherNumber = 123.9523
-// console.log(otherNumber.toPrecision(5))
+const otherNumber = 123.9523
+console.log(otherNumber.toPrecision(5))
 
-// const hundreds = 10000000
-// console.log(hundreds.toLocaleString('en-IN'))
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'))
 
 // Math
 
-// console.log(Math)
-// console.log(Math.abs(-4))
-// console.log(Math.round(4.56))
-// console.log(Math.ceil(5.2))
-// console.log(Math.floor(5.2))
-// console.log(Math.max(5, 6, 2, 7))
-// console.log(Math.min(5, 6, 2, 7))
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.56))
+console.log(Math.ceil(5.2))
+console.log(Math.floor(5.2))
+console.log(Math.max(5, 6, 2, 7))
+console.log(Math.min(5, 6, 2, 7))
 
 console.log(Math.random())
 console.log((Math.random()*10) + 1)
