7da6c2d3e8bc8d6799e4ae03fb1b0ac63201f1eb
Ashu <ayushprajapati272007@gmail.com>
2026-07-03 10:34:10 +0530

    Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,56 @@
+// if
+const isUserloggedIn = true
+const temperature = 51
+
+// if (temperature < 50){
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("Greater than 50")
+// }
+
+// <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+
+// console.log(`User power: ${power}`)
+
+// DON'T
+
+// const balance = 500
+// if(balance > 500) console.log("test"),console.log("test2"); 
+
+
+// const balance = 1000
+
+// if (balance < 500){
+//     console.log("less than 500")
+// } else if (balance < 750){
+//     console.log("less than 750")
+
+// } else if (balance < 950 ){
+//     console.log("less than 950")
+
+// } else{
+//     console.log("less than 1200")
+// }
+
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggesInFromEmail = true
+
+if (userLoggedIn && debitcard && 2==3){
+    console.log(("Allow to buy course"))
+}
+
+if(loggedInFromGoogle || loggesInFromEmail) {
+    console.log("User logged in")
+}
+
