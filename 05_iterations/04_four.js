580f1e9dea46fefdbb2574e87a9c2365124cb44d
Ashu <ayushprajapati272007@gmail.com>
2026-07-03 12:59:38 +0530

    Iterations

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: "Javascript",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "Swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(`Language at ${key} is ${programming[key]}`)
+// }
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+// for (const key in map){
+//     console.log(key)
+// }
+
