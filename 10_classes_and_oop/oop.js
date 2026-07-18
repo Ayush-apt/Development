1f84ee24660f54f189cd0c51b9d1f4a4f3458f7c
Ashu <ayushprajapati272007@gmail.com>
2026-07-18 16:57:31 +0530

    OOP

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,35 @@
+const user = {
+    username: "Ashu",
+    loginCount: 20,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+// console.log(user.username)
+// console.log(user.getUserDetails());
+// console.log(this)
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`)
+    }
+
+    return this
+}
+
+const userOne = new User("Ashu", 20, true)
+const userTwo = new User("Arya", 11, false)
+console.log(userOne.constructor);
+console.log(userTwo);
+
+
